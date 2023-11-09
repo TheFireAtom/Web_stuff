5d324256a4362796ca76732160b68278da4754bc
Ivan Kuleshov <Ivaner1234@yandex.ru>
2023-11-09 14:50:36 +0300

    Added remove quote button

--- JavaScript_DOM_Manipulation course/Second_Pard(Practice)/Project1/project-1.js
+++ JavaScript_DOM_Manipulation course/Second_Pard(Practice)/Project1/project-1.js
@@ -4,10 +4,10 @@
 
 const quotes = ["\"You have power over your mind – not outside events. Realize this, and you will find strength.\"",
 "\"We suffer more often in imagination than in reality.\"",
-"No man is free who is not master of himself.",
+"\"No man is free who is not master of himself.\"",
 "\"Man conquers the world by conquering himself.\"",
-"\"Since every man dies, it is better to die with distinction than to live long.\""]
-const persons = ["Marcus Aurelius", "Seneca", "Epictetus", "Zeno", "Musonius Rufus"]
+"\"Since every man dies, it is better to die with distinction than to live long.\""];
+const persons = ["Marcus Aurelius", "Seneca", "Epictetus", "Zeno", "Musonius Rufus"];
 
 // constants
 
@@ -26,6 +26,7 @@ console.log(button);
 let counter = 0;
 
 button.addEventListener("click", function () {
+
     if (counter < quotes.length) {
         let currentQuote = quotes[counter];
         quote.innerText = currentQuote;
@@ -46,17 +47,39 @@ button.addEventListener("click", function () {
 const addQuoteButton = document.querySelector("#new-user-quote");
 
 addQuoteButton.addEventListener("click", function () {
+
     let userPerson = window.prompt("Enter an author name:");
+    if (userPerson) {
+        persons.push(userPerson);
+    }
+
     let userQuote = window.prompt("Enter a quote:");
-    persons.push(userPerson);
-    quotes.push(userQuote);
-})
+    if (userQuote) {
+        quotes.push(userQuote);
+    }
+});
 
 const deleteQuoteButton = document.querySelector("#delete-user-quote");
 
-addQuoteButton.addEventListener("click", function () {
-    let userPerson = window.prompt("Enter an author name:");
-    let userQuote = window.prompt("Enter a quote:");
-    persons.push(userPerson);
-    quotes.push(userQuote);
-})
+deleteQuoteButton.addEventListener("click", function () {
+
+    let userPerson = window.prompt("Enter the name of the author whose quote you want to remove: ");
+    let userQuote = window.prompt("Enter the quote of the author that you want to remove: ");
+
+    for (let i = 0; i < quotes.length; i++) {
+
+        if (userPerson == persons[i] && userQuote == quotes[i]) {
+            persons.splice(i, 1);
+            quotes.splice(i, 1);
+            window.alert("Done!!!");
+        }
+
+        // else {
+        //     window.alert("Try again!!!");
+        //     break;
+        // }
+
+    }
+
+});
+
