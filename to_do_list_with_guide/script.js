0787917800385df46d504cb5dde9f22356d6b44e
Ivan Kuleshov <Ivaner1234@yandex.ru>
2023-11-24 10:03:56 +0300

    Completed to-do list with guide

--- to_do_list_with_guide/script.js
+++ to_do_list_with_guide/script.js
@@ -1,1 +1,50 @@
 //  Global Script
+
+// constants
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+// Function for adding tasks
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!!!");
+    }
+
+    else {
+        let li = document.createElement("li");
+        li.innerText = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+// EventListener for crossing and deleteng tasks
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+// Fucntion for storing data in browser
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showList() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showList();
