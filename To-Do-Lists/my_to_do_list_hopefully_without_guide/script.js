5749f9adde079a8efdca0c95b827b81c36deb788
Ivan Kuleshov <Ivaner1234@yandex.ru>
2023-11-25 10:21:57 +0300

    Function for adding tasks (with x-marks)

--- To-Do-Lists/my_to_do_list_hopefully_without_guide/script.js
+++ To-Do-Lists/my_to_do_list_hopefully_without_guide/script.js
@@ -1,2 +1,27 @@
 // Main script
 
+// Constants
+
+const tasks = document.querySelector("#tasks");
+console.log(tasks);
+const input = document.querySelector("#text-input");
+console.log(input);
+const addBtn = document.querySelector("#add-button");
+console.log(addBtn);
+
+// Function for adding tasks
+
+function addTask() {
+    let inputText = input.value;
+    let li = document.createElement("li");
+    li.innerText = inputText;
+    let span = document.createElement("span");
+    span.innerText = "❌";
+    li.append(span);
+    tasks.appendChild(li);
+
+}
+
+// addBtn.addEventListener("click", function(e) {
+//     if (e.currentTarget === "")
+// });
