645c439b5b221f37d37c8006568ef725b6d916a8
Ivan Kuleshov <64702232+TheFireAtom@users.noreply.github.com>
2023-11-30 10:43:25 +0300

    food spawn (with border!!!)

--- Projects/Snake_game/Simple_snake_game(my_version)/script.js
+++ Projects/Snake_game/Simple_snake_game(my_version)/script.js
@@ -11,14 +11,13 @@ const unitSize = 25;
 const foodSize = unitSize;
 const canvasWidth = canvas.offsetWidth - 10; // because 10 is border size, 5px from right and 5px from left
 const canvasHeight = canvas.offsetHeight - 10;
-console.log(canvasWidth);
 const xVelocity = unitSize;
 const yVelocity = 0;
 // Color constants
 const innerSnakeColor = "#F05941";
-const outerSnakeColor = "#22092C"
-const innerfoodColor = "#F05941";
-const outerFoodColor = "#22092C";
+const outerSnakeColor = "#000000"
+const innerFoodColor = "#830C24";
+const outerFoodColor = "#000000";
 // Snake constant (here i use an array of objects to draw a snake)
 const snake = [
     {x:unitSize * 5, y:unitSize},
@@ -30,11 +29,23 @@ const snake = [
 // Boolean for gameOver 
 let running = true;
 
+drawFood();
+
 // Functions
 function drawFood() {
-    function placeFood(min, max) {
-        let randomNumber = ((Math.random() * (max - min) + min) / unitSize) * unitSize;
+    function placeInnerFood(min, max) {
+        let randomNumber = (Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize);
         return randomNumber;
     };
-    
+    // Inner food placing
+    let tempFoodLocation = placeInnerFood(0, canvasWidth);
+    context.fillStyle = innerFoodColor;
+    let innerRect = context.fillRect(tempFoodLocation, tempFoodLocation, unitSize, unitSize);
+    context.strokeStyle = outerFoodColor;
+    let outerRect = context.strokeRect(tempFoodLocation, tempFoodLocation, unitSize, unitSize);
+
+    // context.fillRect(placeinnerFood(0, canvasWidth), placeinnerFood(0, canvasHeight), unitSize, unitSize);
+    // Outer food (border) placing
+    // context.fillStyle = outerFoodColor;
+    // context.strokeRect(placeinnerFood(0, canvasWidth), placeinnerFood(0, canvasHeight), unitSize, unitSize);
 };
